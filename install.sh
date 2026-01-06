478739cd14a5f1d497291d019d5647532f2167b9
Victor Pavlychko <victor.pavlychko@grammarly.com>
2026-01-06 15:24:30 -0800

    Initial nvim conf

--- install.sh
+++ install.sh
@@ -18,6 +18,14 @@ function install_root_module() {
     done
 }
 
+function install_config_module() {
+    mkdir -p "${HOME}/.config"
+
+    echo "Symlinking $1"
+    ln -sf "$(realpath $1)" "${HOME}/.config/$1"
+}
+
 install_root_module zsh
 install_root_module tmux
+install_config_module nvim
 
---
+++ nvim/lua/conf/init.lua
@@ -0,0 +1,4 @@
+-- Set leader key first (related to Lazy plugin manager)
+-- vim.g.mapleader = " "
+ 
+require("conf.settings")
